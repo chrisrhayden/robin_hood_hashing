2ac37c303f0cacc453aba8a6a5c5179c85407b0a
chrisrhayden <chrisrhayden@gmail.com>
2022-05-24 21:20:44 -0700

    Change insert to use just the map array; fix logic bug for inserting

--- src/robin_hood.c
+++ src/robin_hood.c
@@ -14,7 +14,7 @@ void swap(void **a, void **b) {
   *b = temp;
 }
 
-Map *_create_map(hash_func_t hash, compare_func_t comp, uint64_t size) {
+Map *_create_map(hash_func_t hash, uint64_t size) {
   Map *map = malloc(sizeof(Map));
   map->buckets = (Item **)malloc(sizeof(Item *) * size);
   map->bucket_len = size;
@@ -25,13 +25,12 @@ Map *_create_map(hash_func_t hash, compare_func_t comp, uint64_t size) {
   }
 
   map->hash_func = hash;
-  map->compare_func = comp;
 
   return map;
 }
 
-Map *create_map(hash_func_t hash, compare_func_t comp) {
-  return _create_map(hash, comp, STARTING_SIZE);
+Map *create_map(hash_func_t hash) {
+  return _create_map(hash, STARTING_SIZE);
 }
 
 Item *create_item(void *key, void *value, uint64_t hash, uint64_t probe) {
@@ -45,56 +44,33 @@ Item *create_item(void *key, void *value, uint64_t hash, uint64_t probe) {
   return item;
 }
 
-bool rehash_map(Map *map) {
-  printf("rehash\n");
-  uint64_t new_size = map->bucket_len * GROWTH_FACTOR;
+bool _insert_value(Item **buckets, uint64_t bucket_len, hash_func_t hash_func,
+                   void *key, void *value) {
 
-  Map *new_map = _create_map(map->hash_func, map->compare_func, new_size);
+  uint64_t hash = hash_func(key);
+  // uint64_t pos = hash & (bucket_len - 1);
+  uint64_t pos = hash % bucket_len;
 
-  if (new_map == NULL) {
-    return false;
-  }
-
-  Item *item = NULL;
-  for (uint64_t i = 0; i < map->bucket_len; i++) {
-    item = map->buckets[i];
-
-    if (item != NULL) {
-      insert_value(new_map, item->key, item->value);
-    }
-  }
-
-  free(map->buckets);
-  map->buckets = new_map->buckets;
-  map->bucket_len = new_map->bucket_len;
-
-  return true;
-}
-
-bool insert_value(Map *map, void *key, void *value) {
-  if (map->bucket_load + 1 == floor(map->bucket_len * MAX_LOAD_FACTOR)) {
-    if (rehash_map(map) == false) {
-      return false;
-    }
-  }
-
-  uint64_t hash = map->hash_func(key);
-  uint64_t pos = hash % map->bucket_len;
+  uint64_t current_pos = pos;
 
   uint64_t probe = 0;
-
   Item *item = NULL;
 
+  bool success = true;
   bool finding_bucket = true;
 
   while (finding_bucket) {
-    uint64_t current_pos = (pos + probe) % map->bucket_len;
 
-    item = map->buckets[current_pos];
+    item = buckets[current_pos];
 
     if (item == NULL) {
-      // TODO: check for null
-      map->buckets[current_pos] = create_item(key, value, hash, probe);
+      Item *new_item = create_item(key, value, hash, probe);
+
+      if (new_item == NULL) {
+        success = false;
+      }
+
+      buckets[current_pos] = new_item;
 
       finding_bucket = false;
     } else {
@@ -110,16 +86,59 @@ bool insert_value(Map *map, void *key, void *value) {
         uint64_t temp_probe = item->probe;
         item->probe = probe;
         probe = temp_probe;
-
-      } else {
-        probe++;
       }
+
+      current_pos = (current_pos + 1) % bucket_len;
+      probe++;
     }
   }
 
-  map->bucket_load++;
+  return success;
+}
+
+bool rehash_map(Map *map) {
+  uint64_t new_len = map->bucket_len * GROWTH_FACTOR;
+
+  Item **new_buckets = (Item **)malloc(sizeof(Item *) * new_len);
+
+  if (new_buckets == NULL) {
+    return false;
+  }
+
+  bool success = true;
+
+  Item *item = NULL;
+  for (uint64_t i = 0; i < map->bucket_len && success; i++) {
+    item = map->buckets[i];
+
+    if (item != NULL) {
+      success = _insert_value(new_buckets, new_len, map->hash_func, item->key,
+                              item->value);
+    }
+  }
+
+  free(map->buckets);
+  map->buckets = new_buckets;
+  map->bucket_len = new_len;
 
   return true;
+}
+
+bool insert_value(Map *map, void *key, void *value) {
+  if (map->bucket_load + 1 == floor(map->bucket_len * MAX_LOAD_FACTOR)) {
+    if (rehash_map(map) == false) {
+      return false;
+    }
+  }
+
+  if (_insert_value(map->buckets, map->bucket_len, map->hash_func, key,
+                    value)) {
+    map->bucket_load++;
+
+    return true;
+  }
+
+  return false;
 }
 
 void backward_shift(Map *map, uint64_t pos) {
@@ -130,7 +149,7 @@ void backward_shift(Map *map, uint64_t pos) {
   bool move_backward = true;
 
   while (move_backward) {
-    current_pos++;
+    current_pos = (current_pos + 1) % map->bucket_len;
     item = map->buckets[current_pos];
 
     if (item == NULL || item->probe == 0) {
@@ -139,15 +158,13 @@ void backward_shift(Map *map, uint64_t pos) {
       item->probe--;
       map->buckets[current_pos - 1] = item;
       map->buckets[current_pos] = NULL;
-      printf("move back key: %lu old probe: %lu new probe: %lu\n",
-             *(uint64_t *)item->key, item->probe + 1, item->probe);
     }
   }
 }
 
 Item *delete_item(Map *map, void *key) {
   uint64_t hash = map->hash_func(key);
-  uint64_t pos = hash & (map->bucket_len - 1);
+  uint64_t pos = hash % map->bucket_len;
   uint64_t probe = 0;
 
   Item *item = NULL;
@@ -156,12 +173,14 @@ Item *delete_item(Map *map, void *key) {
   bool finding_bucket = true;
 
   while (finding_bucket) {
-    current_pos = pos + probe;
+    current_pos = (pos + probe) % map->bucket_len;
+
     item = map->buckets[current_pos];
 
-    if (item == NULL) {
+    if (item == NULL || probe > item->probe) {
+      item = NULL;
       finding_bucket = false;
-    } else if (map->compare_func(key, item->key)) {
+    } else if (item->hash == hash) {
       finding_bucket = false;
       map->buckets[current_pos] = NULL;
 
@@ -176,7 +195,7 @@ Item *delete_item(Map *map, void *key) {
 
 Item *lookup_key(Map *map, void *key) {
   uint64_t hash = map->hash_func(key);
-  uint64_t pos = hash & (map->bucket_len - 1);
+  uint64_t pos = hash % map->bucket_len;
   uint64_t probe = 0;
 
   Item *item = NULL;
@@ -192,11 +211,11 @@ Item *lookup_key(Map *map, void *key) {
       finding_bucket = false;
     } else {
       // if this is true then we found the correct bucket
-      if (map->compare_func(key, item->key)) {
+      if (item->hash == hash) {
         finding_bucket = false;
 
-        // if the item probe is larger the we would have found the key we are
-        // looking for by now
+        // if the item probe is larger the we would have found the key
+        // we are looking for by now
       } else if (probe > item->probe) {
         item = NULL;
         finding_bucket = false;
@@ -216,8 +235,8 @@ IterMap *create_iter_map(Map *map) {
     return NULL;
   }
 
-  iter_map->buckets = map->buckets;
   iter_map->current_pos = 0;
+  iter_map->buckets = map->buckets;
   iter_map->bucket_len = map->bucket_len;
 
   return iter_map;
@@ -228,18 +247,14 @@ Item *next_item(IterMap *iter_map) {
 
   bool looking = true;
 
-  while (looking) {
-    if (iter_map->current_pos >= iter_map->bucket_len) {
+  while (looking && iter_map->current_pos <= iter_map->bucket_len) {
+    item = iter_map->buckets[iter_map->current_pos];
+
+    if (item != NULL) {
       looking = false;
-    } else {
-      item = iter_map->buckets[iter_map->current_pos];
-
-      if (item != NULL) {
-        looking = false;
-      }
-
-      iter_map->current_pos++;
     }
+
+    iter_map->current_pos++;
   }
 
   return item;
